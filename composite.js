470d685a1c4d79b54c7913714e87de2ee4088f45
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2021-05-11 17:19:58 +0700

    update

--- composite.js
+++ composite.js
@@ -1,20 +1,45 @@
 var jimp = require('jimp');
+const express = require('express');
+const app = express();
+const port = 3003;
+var path = require('path');
+var bodyParser = require('body-parser');
 
-var images = ['images/background.jpg', 'images/banner.png', 'images/logo.png'];
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var jimps = [];
+app.get('/', (req, res) => {
+  let { x, y } = req.query;
+  x = parseInt(x);
+  y = parseInt(y);
+  
+  // var images = ['images/background.jpg', 'images/banner.png', 'images/logo.png'];
+  var images = ['images/background.jpg', 'images/banner.png', 'images/logo.png'];
 
-for (var i = 0; i < images.length; i++) {
-  jimps.push(jimp.read(images[i]));
-}
+  var jimps = [];
 
-Promise.all(jimps).then(function(data) {
-  return Promise.all(jimps);
-}).then(function(data) {
-  data[0].composite(data[1],0,0);
-  data[0].composite(data[2],0,0);
+  for (var i = 0; i < images.length; i++) {
+    jimps.push(jimp.read(images[i]));
+  }
 
-  data[0].write('final-images/test.png', function() {
-    console.log("wrote the image");
-  });
+  Promise.all(jimps)
+    .then(function (data) {
+      return Promise.all(jimps);
+    })
+    .then(function (data) {
+      data[0].composite(data[1], 0, 0);
+      data[0].composite(data[2], 0, 0);
+
+      data[0].write('public/test.png', function () {
+        console.log('wrote the image');
+        res.sendFile(path.join(__dirname, 'public', 'test.png'))
+      });
+    });
+});
+
+app.listen(port, () => {
+  console.log('server start om port ' + port);
 });
